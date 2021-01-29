a2e3a7824b307982eb4fbf0676d98c31a0e1d961
sunxiaoyu <sunxiaoy@cn.ibm.com>
2021-01-29 15:08:57 +0800

    update

--- src/org/iopanda/devops/common/message/Message.groovy
+++ src/org/iopanda/devops/common/message/Message.groovy
@@ -4,17 +4,19 @@ class Message implements Serializable {
     private String color
     private String message
 
+    @NonCPS
     public void setColor(String color){
         this.color = color
     }
+    @NonCPS
     public void setMessage(String message){
         this.message = message
     }
-
+    @NonCPS
     public String getColor(){
         return color
     }
-
+    @NonCPS
     public String getMessage(){
         return message
     }
