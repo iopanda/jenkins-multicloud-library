88da7719a5b2cb7a9f891cd2f8b8bb078a43bf03
sunxiaoyu <sunxiaoy@cn.ibm.com>
2021-01-29 17:19:10 +0800

    update

--- src/org/iopanda/devops/common/Slack.groovy
+++ src/org/iopanda/devops/common/Slack.groovy
@@ -7,9 +7,17 @@ class Slack extends Common {
     private String channel
 
     Slack(steps){ super(steps) }
+    Slack(steps, String channel){
+        super(steps)
+        this.channel = channel
+    }
 
     void send(Message message) {
+        send(channel, message)
+    }
+
+    void send(String ch, Message message){
         message.setSteps(steps)
-        slackSend(channel: "${channel}", color: "${message.getColor()}", message: "${message.getMessage()}")
+        slackSend(channel: "${ch}", color: "${message.getColor()}", message: "${message.getMessage()}")
     }
 }
